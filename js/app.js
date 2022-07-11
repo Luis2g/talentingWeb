84aa9af61630742c31f238767d52346634521b70
Luis2g <luisjimenez2g@gmail.com>
2022-07-10 20:57:48 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var talenting = angular.module('talenting',['ngRoute']);
+
+talenting.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider
+        .when('/', {
+            templateUrl: '/views/index.html'
+        })
+        .when('/userRegistration', {
+            templateUrl: '/views/users/userRegistration.html',
+            controller: 'userController'
+        })
+        .when('/404', {
+            templateUrl: "/views/errorPages/404.html"
+        })
+        .otherwise({
+            redirectTo: '/404'
+        });
+
+}]);
