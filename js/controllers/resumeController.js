e623184c80c4403c10fc5eb5ade076bf36eb3c82
DiegoVelascoGH <20193tn054@utez.edu.mx>
2022-08-21 22:43:33 -0500

    [ADD] Adding view CV

--- js/controllers/resumeController.js
+++ js/controllers/resumeController.js
@@ -5,7 +5,7 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
 
     $scope.languages = ["Español","Inglés", "Francés", "Alemán", "Chino", "Portugués", "Ruso", "Italiano", "Indu", "Árabe", "Japonés", "Coreano", "Otro"];
     
-    $scope.languagesLevel = ["A1","A2", "B1", "B2","C1","C2","Nativo"];
+    $scope.languagesLevel = ["Nativo","A1","A2", "B1", "B2","C1","C2"];
 
     $scope.typeCourse= ["Curso","Certificación"];
 
@@ -72,7 +72,12 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
                 $scope.resume = response.data.resume;
                 $scope.showHabilities = true;
                 $scope.showLanguages = true;
-                if(response.data.certificationOrCourse !== null){
+                
+                    document.getElementById("ProfileImagePreview").src = "data:image/png;base64," + response.data.resume.profileImage;
+
+                    document.getElementById("previewPDF").src = "data:application/pdf;base64," + response.data.resume.pdfresume;
+
+                if(response.data.certificationOrCourse !== null ){
                     $scope.listCourses = response.data.certificationOrCourse;
                     $scope.showCourses = true;
                 }else{
@@ -328,8 +333,8 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
                     $scope.formResume.expertise.$error.required
                     $scope.formResume.schoolPreparation.$error.required
 
-                    await this.uploadProfileImage();
-                    await this.uploadPDFResume(); 
+                    // await this.uploadProfileImage();
+                    // await this.uploadPDFResume(); 
                     $http({
                         method: 'POST',
                         url: 'http://localhost:8080/talenting/resumes',
@@ -343,8 +348,7 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
                             showConfirmButton: false,
                             timer: 5000
                         }).then( () => {
-                        //   window.location.replace('/');
-                        //   $scope.$apply();
+                            window.location.reload();
                         });
                       }, err => {
                         
@@ -388,8 +392,8 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
                     $scope.formResume.schoolPreparation.$error.required
                     
                     console.log(resumeDTO);
-                    await this.uploadProfileImage();
-                    await this.uploadPDFResume(); 
+
+
                     $http({
                         method: 'POST',
                         url: 'http://localhost:8080/talenting/resumes',
@@ -403,8 +407,7 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
                             showConfirmButton: false,
                             timer: 5000
                         }).then( () => {
-                            window.location.replace('/');
-                            $scope.$apply();
+                            window.location.reload();
                         });
                       }, err => {
                         
@@ -441,154 +444,74 @@ talenting.controller('resumeController', ['$scope', '$http', '$location','userSe
         });
     }
 
-    this.uploadProfileImage = async() => {
-        let picture = document.getElementById("profileImage").files[0];
-        var formData = new FormData();
-        if (picture) {
-            formData.append("file", picture);
-            let res = await fetch("http://localhost:8080/talenting/upload/profileImage", {
-                method: "POST",
-                body: formData,
-            }).then((r) => {
-                $scope.uploadedPic = true;
-            });
+    let inputImage = document.getElementById("profileImage")
+    let inputPFDResume = document.getElementById("PDFResume")
+    
+
+    inputImage.addEventListener('change', (e) => {
+    let reader = new FileReader();
+    let profileImageByteArray = [];
+    $scope.imagePreview = true;
+    reader.readAsArrayBuffer(e.target.files[0]);
+    reader.onloadend = (evt) => {
+            if (evt.target.readyState === FileReader.DONE) {
+            const arrayBufferImage = evt.target.result,
+                array = new Uint8Array(arrayBufferImage);
+            for (const a of array) {
+                profileImageByteArray.push(a);
+            }
+            $scope.resume.profileImage = profileImageByteArray;
+            let binaryImage = '';
+            let bytesImage = new Uint8Array(profileImageByteArray);
+            let lenImage = bytesImage.byteLength;
+            for (let i = 0; i < lenImage; i++) {
+                binaryImage += String.fromCharCode(bytesImage[i]);
+            }
+            let file = window.btoa(binaryImage);
+            document.getElementById("ProfileImagePreview").src = "data:image/png;base64," + file;
+            }
         }
-    };
+    })
 
-    this.uploadPDFResume = async() => {
-        let picture = document.getElementById("PDFResume").files[0];
-        var formData = new FormData();
-        if (picture) {
-            formData.append("file", picture);
-            let res = await fetch("http://localhost:8080/talenting/upload/PDFResume", {
-                method: "POST",
-                body: formData,
-            }).then((r) => {
-                $scope.uploadedPic = true;
-            });
-        }
-    };
+    inputPFDResume.addEventListener('change', (e) => {
+        $scope.PDFResumePreview = true;
+        let reader = new FileReader();
+        let PDFResumeByteArray = [];
+        reader.readAsArrayBuffer(e.target.files[0]);
+        reader.onloadend = (evt) => {
+                if (evt.target.readyState === FileReader.DONE) {
+                const arrayBuffer = evt.target.result,
+                    array = new Uint8Array(arrayBuffer);
+                for (const a of array) {
+                    PDFResumeByteArray.push(a);
+                }
+                $scope.resume.pdfresume = PDFResumeByteArray;
+                let binary = '';
+                let bytes = new Uint8Array($scope.resume.pdfresume);
+                let len = bytes.byteLength;
+                for (let i = 0; i < len; i++) {
+                    binary += String.fromCharCode(bytes[i]);
+                }
+                let filePDF = window.btoa(binary);
+                document.getElementById("previewPDF").src = "data:application/pdf;base64," + filePDF;
+                }
+            }
+        })
 
-    $scope.generateCV = () => {
-        var pdfObject = jsPDFInvoiceTemplate.default(props)
-
+    $scope.generateResume = () => {
+        return $http({
+            method: "GET",
+            url: 'http://localhost:8080/talenting/resumes/CV/'+$scope.resume.id,
+            responseType: "arraybuffer",
+        }).then((res) => {
+            const fileName = $scope.resume.person.name + " " + $scope.resume.person.surname + " " + $scope.resume.person.secondSurname + " CV";
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            const file = new Blob([res.data], {type: "application/pdf"});
+            a.href = window.URL.createObjectURL(file);
+            a.download = fileName;
+            a.click();
+        })
     }
-
-    // var props = {
-    //     outputType: jsPDFInvoiceTemplate.OutputType.Save,
-    //     returnJsPDFDocObject: true,
-    //     fileName: "Invoice 2021",
-    //     orientationLandscape: false,
-    //     compress: true,
-    //     logo: {
-    //         src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
-    //         type: 'PNG', //optional, when src= data:uri (nodejs case)
-    //         width: 53.33, //aspect ratio = width/height
-    //         height: 26.66,
-    //         margin: {
-    //             top: 0, //negative or positive num, from the current position
-    //             left: 0 //negative or positive num, from the current position
-    //         }
-    //     },
-    //     stamp: {
-    //         inAllPages: true, //by default = false, just in the last page
-    //         src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
-    //         type: 'JPG', //optional, when src= data:uri (nodejs case)
-    //         width: 20, //aspect ratio = width/height
-    //         height: 20,
-    //         margin: {
-    //             top: 0, //negative or positive num, from the current position
-    //             left: 0 //negative or positive num, from the current position
-    //         }
-    //     },
-    //     business: {
-    //         name: "Business Name",
-    //         address: "Albania, Tirane ish-Dogana, Durres 2001",
-    //         phone: "(+355) 069 11 11 111",
-    //         email: "email@example.com",
-    //         email_1: "info@example.al",
-    //         website: "www.example.al",
-    //     },
-    //     contact: {
-    //         label: "Invoice issued for:",
-    //         name: "Client Name",
-    //         address: "Albania, Tirane, Astir",
-    //         phone: "(+355) 069 22 22 222",
-    //         email: "client@website.al",
-    //         otherInfo: "www.website.al",
-    //     },
-    //     invoice: {
-    //         label: "Invoice #: ",
-    //         num: 19,
-    //         invDate: "Payment Date: 01/01/2021 18:12",
-    //         invGenDate: "Invoice Date: 02/02/2021 10:17",
-    //         headerBorder: false,
-    //         tableBodyBorder: false,
-    //         header: [
-    //           {
-    //             title: "#", 
-    //             style: { 
-    //               width: 10 
-    //             } 
-    //           }, 
-    //           { 
-    //             title: "Title",
-    //             style: {
-    //               width: 30
-    //             } 
-    //           }, 
-    //           { 
-    //             title: "Description",
-    //             style: {
-    //               width: 80
-    //             } 
-    //           }, 
-    //           { title: "Price"},
-    //           { title: "Quantity"},
-    //           { title: "Unit"},
-    //           { title: "Total"}
-    //         ],
-    //         table: Array.from(Array(10), (item, index)=>([
-    //             index + 1,
-    //             "There are many variations ",
-    //             "Lorem Ipsum is simply dummy text dummy text ",
-    //             200.5,
-    //             4.5,
-    //             "m2",
-    //             400.5
-    //         ])),
-    //         additionalRows: [{
-    //             col1: 'Total:',
-    //             col2: '145,250.50',
-    //             col3: 'ALL',
-    //             style: {
-    //                 fontSize: 14 //optional, default 12
-    //             }
-    //         },
-    //         {
-    //             col1: 'VAT:',
-    //             col2: '20',
-    //             col3: '%',
-    //             style: {
-    //                 fontSize: 10 //optional, default 12
-    //             }
-    //         },
-    //         {
-    //             col1: 'SubTotal:',
-    //             col2: '116,199.90',
-    //             col3: 'ALL',
-    //             style: {
-    //                 fontSize: 10 //optional, default 12
-    //             }
-    //         }],
-    //         invDescLabel: "Invoice Note",
-    //         invDesc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
-    //     },
-    //     footer: {
-    //         text: "The invoice is created on a computer and is valid without the signature and stamp.",
-    //     },
-    //     pageEnable: true,
-    //     pageLabel: "Page ",
-    // };
 
 }]);
