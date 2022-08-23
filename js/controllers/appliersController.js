9faa6045a84122034b624fe45d9c5b671bd595d1
Daniel Rodriguez <20193tn018@utez.edu.mx>
2022-08-23 18:56:51 -0500

    [ADD] Send emails plain text

--- js/controllers/appliersController.js
+++ js/controllers/appliersController.js
@@ -1,7 +1,7 @@
 talenting.controller('appliersController',
     ['$scope', '$http', '$location', 'userService', '$cookies', 'alertService',
         function ($scope, $http, $location, userService, $cookies, alertService) {
-
+            $scope.email;
             $scope.vacancy = JSON.parse(localStorage.getItem('vacancy'));
             $scope.appliers = {};
             $scope.applierInterview = [];
@@ -39,6 +39,29 @@ talenting.controller('appliersController',
                         params: { status: status, id: id }
                     }).then(response => {
                         $scope.loadAppliersByVacancy();
+                        let company = $scope.vacancy.employeer.companyName
+                        $scope.email = $scope.vacancy.employeer.companyName
+                        let mail = {
+                            to: applier.person.contactInformation.email,
+                            subject: 'Notificación Talenting.',
+                            content: 'Tu Curriculim Vitae ha sido visualizado por la empresa '+company+' a la cual te has postulado.', 
+                        }
+                        $http({
+                            method: 'POST',
+                            url: 'http://localhost:8080/talenting/sendNotification',
+                            data: mail
+                        }).then(response => {
+
+                            let company = $scope.vacancy.employeer.companyName
+    
+                            let mail = {
+                                to: applier.person.contactInformation.email,
+                                subject: 'Notificación Talenting.',
+                                content: 'Tu Curriculim Vitae ha sido visualizado por la empresa '+company+' a la cual te has postulado.', 
+                            }
+                        }, err => {
+                        });
+                        
                     }, err => {
                     });
                 }
@@ -65,6 +88,51 @@ talenting.controller('appliersController',
                         }).then(response => {
                             alertService.showAlert.success('Se ha modificado el estado de la solicitud exitosamente');
                             $scope.loadAppliersByVacancy();
+                            let company = $scope.vacancy.employeer.companyName
+                            if(status === 'Rechazado'){
+                                let mail = {
+                                    to: applier.person.contactInformation.email,
+                                    subject: 'Notificación Talenting.',
+                                    content: 'Tu solicitud para la vacante de la empresa '+company+' a la cual te has postulado, ha sido rechazada.', 
+                                }
+                                $http({
+                                    method: 'POST',
+                                    url: 'http://localhost:8080/talenting/sendNotification',
+                                    data: mail
+                                }).then(response => {
+    
+                                    let company = $scope.vacancy.employeer.companyName
+            
+                                    let mail = {
+                                        to: applier.person.contactInformation.email,
+                                        subject: 'Notificación Talenting.',
+                                        content: 'Tu Curriculim Vitae ha sido visualizado por la empresa '+company+' a la cual te has postulado.', 
+                                    }
+                                }, err => {
+                                });
+                            }else if(status ==='Contratado'){
+                                let mail = {
+                                    to: applier.person.contactInformation.email,
+                                    subject: 'Notificación Talenting.',
+                                    content: '¡Felicidades!, te informamos que la empresa '+company+' te ha contratado, se contactarán contigo lo más pronto posible', 
+                                }
+                                $http({
+                                    method: 'POST',
+                                    url: 'http://localhost:8080/talenting/sendNotification',
+                                    data: mail
+                                }).then(response => {
+    
+                                    let company = $scope.vacancy.employeer.companyName
+            
+                                    let mail = {
+                                        to: applier.person.contactInformation.email,
+                                        subject: 'Notificación Talenting.',
+                                        content: 'Tu Curriculim Vitae ha sido visualizado por la empresa '+company+' a la cual te has postulado.', 
+                                    }
+                                }, err => {
+                                });
+                            }
+                            
                         }, err => {
                             alertService.showAlert.error('Ocurrio un error al cambiar el estado de la solicitud');
                         });
@@ -82,12 +150,21 @@ talenting.controller('appliersController',
 
             $scope.openModalInterview = (applier) => {
                 $scope.applierInterview = applier;
+                $scope.email = applier.person.contactInformation.email
                 $scope.modal = new bootstrap.Modal(document.getElementById("interviewModal"), {});
                 $scope.interviewModalRegister.$setUntouched();
                 $scope.interviewModalRegister.$setPristine();
 
                 $scope.modal.show();
             };
+
+            $scope.formatDate = (date) => {
+                let ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(date);
+                let mo = new Intl.DateTimeFormat('en', {month: '2-digit'}).format(date);
+                let da = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(date);
+                return  ye + "-" + mo + "-" + da;
+            }
+
 
             $scope.setInterviewDate = () => {
                 let date = new Date($scope.interviewDate);
@@ -99,6 +176,28 @@ talenting.controller('appliersController',
                     data: $scope.applierInterview
                 }).then(response => {
                     alertService.showAlert.success('Se ha registrado la entrevista exitosamente');
+                    let company = $scope.vacancy.employeer.companyName
+                    let fechaAgendada = $scope.formatDate($scope.interviewDate)
+                    let mail = {
+                            to: $scope.email,
+                            subject: 'Notificación Talenting.',
+                            content: 'La empresa '+company+' ha agendado una entravista de trabajo para la fecha: '+fechaAgendada, 
+                        }
+                        $http({
+                            method: 'POST',
+                            url: 'http://localhost:8080/talenting/sendNotification',
+                            data: mail
+                        }).then(response => {
+
+                            let company = $scope.vacancy.employeer.companyName
+    
+                            let mail = {
+                                to: applier.person.contactInformation.email,
+                                subject: 'Notificación Talenting.',
+                                content: 'Tu Curriculim Vitae ha sido visualizado por la empresa '+company+' a la cual te has postulado.', 
+                            }
+                        }, err => {
+                        });
                     $scope.interviewDate = "";
                     $scope.modal.hide();
                 }, err => {
